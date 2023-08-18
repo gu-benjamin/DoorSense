6df24f1b7c6399c268184fdecc8673b2b86f8646
gustavinxd <gustasbenjamin@gmail.com>
2023-08-18 17:08:01 -0300

    Modal de testes criada

--- src/app/(authenticated)/Dashboard/page.tsx
+++ src/app/(authenticated)/Dashboard/page.tsx
@@ -2,12 +2,13 @@
 
 import Image from 'next/image';
 import IconUser from './../../../components/Icons/icon-user';
+import Modal from './../../../components/Modals/modal';
 
 export default function HomePage() {
 
   return (
     <main className={`w-full h-full flex justify-center items-center`}>
-      <h1>Teste</h1>
+     <Modal/>
     </main>
   );
 }
