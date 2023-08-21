57bb9e6e60703b08c728578562e03446c4f084b2
Joao Pedro <114251012+joaopedrojesus1@users.noreply.github.com>
2023-08-21 16:05:39 -0300

    mudei a responsividade e adicionei coisas

--- src/app/(authenticated)/Dashboard/page.tsx
+++ src/app/(authenticated)/Dashboard/page.tsx
@@ -5,6 +5,8 @@ import IconHome from './../../../components/Icons/🦆 icon-home';
 import IconMais from './../../../components/Icons/icon-mais';
 import IconEdit from './../../../components/Icons/🦆 icon-edit';
 import IconTrash from './../../../components/Icons/🦆 icon-trash';
+import IconCerto from 'components/Icons/icon-certo';
+import IconX from 'components/Icons/🦆 icon-x';
 
 export default function HomePage(){
   
@@ -17,20 +19,22 @@ export default function HomePage(){
         <div className="w-full sm:w-11/12 md:w-5/6 lg:w-3/5 xl:w-4/5 p-4">
           <div className="flex">
             <div className="bg-gray-100 py-4 text-left">
-              <div className="flex items-center">
-                <IconHome size={80} color={`var(--color-primary)`} />
-                <div className="p-5">
-                  <p className="text-5xl font-semibold text-gray-800">Salas</p>
-                  <p className="text-gray-600">Lista das salas criadas</p>
-                </div>
-              </div>
+            <div className="flex items-center">
+            {/* Tamanho responsivo para IconHome */}
+            <IconHome size={75}  />
+            <div className="p-5">
+              {/* Tamanho responsivo para textos */}
+              <p className="text-3xl sm:text-5xl font-semibold text-gray-800">Salas</p>
+              <p className="text-md sm:text-lg text-gray-600">Lista das salas criadas</p>
+            </div>
+          </div>
             </div>
           </div>
 
 
-         {/* Campo de busca e filtro */}
-          <div className="flex items-center mb-4">
-            <div className="relative max-w-4xl w-full">
+          {/* Campo de busca e filtro */}
+          <div className="flex sm:flex-row mb-4">
+            <div className="relative max-w-7xl w-full ">
               <div className="bg-blue-200 p-4 rounded-2xl flex w-full">
                 <input className="pl-10 pr-4 text-base focus:shadow-outline rounded-full w-full" type="search" placeholder="Buscar..." />
                 <button className="ml-10 border-2 border-primary bg-transparent hover:text-white hover:bg-primary text-cyan-500 font-semibold py-2 px-4 rounded">
@@ -40,8 +44,8 @@ export default function HomePage(){
             </div>
             
             {/* Nova Sala */}
-            <div className="bg-blue-200 ml-2 p-4 rounded-2xl flex items-center">
-              <button className=" border-primary bg-primary text-white font-semibold py-2 px-4 rounded flex items-center">
+            <div className="bg-blue-200 p-4 rounded-2xl flex items-center">
+              <button className="border-primary bg-primary text-white font-semibold py-2 px-4 rounded flex items-center" style={{ whiteSpace: 'nowrap' }}>
                 <IconMais size={17} />
                 Nova Sala
               </button>
@@ -50,47 +54,98 @@ export default function HomePage(){
 
 
           {/* O Dashboard */}
-          <div className="bg-white p-4 shadow rounded-lg">
+          <div className="bg-white p-4 shadow rounded-lg overflow-x-auto">
             <h2 className="text-gray-500 text-lg font-semibold pb-4">Salas</h2>
             <div className="my-1 h-px bg-gradient-to-r from-cyan-300 to-cyan-500 mb-6"></div>
             <table className="w-full table-auto text-sm">
-              <thead>
-                <tr className="hover:bg-grey-lighter">
-                  <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light">ID</th>
-                  <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light">Número</th>
-                  <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light">Nome da Sala</th>
-                  <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light">Status</th>
-                  <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light">Ações</th>
-                </tr>
-              </thead>
+            <thead>
+              <tr className="hover:bg-grey-lighter">
+                <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light relative">ID</th>
+                <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light relative">Número</th>
+                <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light relative">Nome da Sala</th>
+                <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light relative">Status</th>
+                <th className="py-2 px-4 bg-grey-lightest font-normal uppercase text-sm text-grey-light border-b border-grey-light relative">Ações</th>
+              </tr>
+            </thead>
               <tbody>
+              {/* -----------------------------ID 1-------------------------------- */}
                 <tr className="hover:bg-grey-lighter">
                   <td className="py-2 px-4 text-center border-b border-grey-light">123abcdef456ghi89</td>
                   <td className="py-2 px-4 text-center border-b border-grey-light">1</td>
                   <td className="py-2 px-4 text-center border-b border-grey-light">Laboratório</td>
-                  <td className="py-2 px-4 text-center border-b border-grey-light text-green-500">Ativo</td>
-                  <td className="flex py-2 px-4 text-center border-b border-grey-light"><IconEdit size={25} /><IconTrash size={40} /></td>
+                  <td className="py-2 px-4 text-center border-b border-grey-light text-green-500">
+                    <div className="flex items-center justify-center">
+                      <IconCerto size={16} />
+                      <span className="ml-1">Ativo</span>
+                    </div>
+                  </td>
+                  <td className="flex items-center justify-center py-2 px-4 text-center border-b border-grey-light">
+                    <div className="flex items-center space-x-2">
+                      <IconEdit size={30} />
+                      <div className="w-px h-7 bg-gradient-to-r from-cyan-300 to-cyan-500"></div>
+                      <IconTrash size={37} />
+                    </div>
+                  </td>
                 </tr>
+
+              {/* -----------------------------ID 2-------------------------------- */}
                 <tr className="hover:bg-grey-lighter">
                   <td className="py-2 px-4 text-center border-b border-grey-light">123abcdef456ghi89</td>
                   <td className="py-2 px-4 text-center border-b border-grey-light">1</td>
                   <td className="py-2 px-4 text-center border-b border-grey-light">Laboratório</td>
-                  <td className="py-2 px-4 text-center border-b border-grey-light text-red-500">Inativo</td>
-                  <td className="flex py-2 px-4 text-center border-b border-grey-light"><IconEdit size={25} /><IconTrash size={40} /></td>
+                  <td className="py-2 px-4 text-center border-b border-grey-light text-red-500">
+                    <div className="flex items-center justify-center">
+                      <IconX size={17} />
+                      <span className="ml-1">Inativo</span>
+                    </div>
+                  </td>
+                  <td className="flex items-center justify-center py-2 px-4 text-center border-b border-grey-light">
+                    <div className="flex items-center space-x-2">
+                      <IconEdit size={30} />
+                      <div className="w-px h-7 bg-gradient-to-r from-cyan-300 to-cyan-500"></div>
+                      <IconTrash size={37} />
+                    </div>
+                  </td>
                 </tr>
+
+              {/* ---------------------------------ID 3---------------------------------- */}
                 <tr className="hover:bg-grey-lighter">
                   <td className="py-2 px-4 text-center border-b border-grey-light">123abcdef456ghi89</td>
                   <td className="py-2 px-4 text-center border-b border-grey-light">1</td>
                   <td className="py-2 px-4 text-center border-b border-grey-light">Laboratório</td>
-                  <td className="py-2 px-4 text-center border-b border-grey-light text-green-500">Ativo</td>
-                  <td className="flex py-2 px-4 text-center border-b border-grey-light"><IconEdit size={25} /><IconTrash size={40} /></td>
+                  <td className="py-2 px-4 text-center border-b border-grey-light text-green-500">
+                    <div className="flex items-center justify-center">
+                      <IconCerto size={16} />
+                      <span className="ml-1">Ativo</span>
+                    </div>
+                  </td>
+                  <td className="flex items-center justify-center py-2 px-4 text-center border-b border-grey-light">
+                    <div className="flex items-center space-x-2">
+                      <IconEdit size={30} />
+                      <div className="w-px h-7 bg-gradient-to-r from-cyan-300 to-cyan-500"></div>
+                      <IconTrash size={37} />
+                    </div>
+                  </td>
                 </tr>
+
+              {/* -------------------------------ID 4-------------------------------- */}
                 <tr className="hover:bg-grey-lighter">
                   <td className="py-2 px-4 text-center border-b border-grey-light">123abcdef456ghi89</td>
                   <td className="py-2 px-4 text-center border-b border-grey-light">1</td>
                   <td className="py-2 px-4 text-center border-b border-grey-light">Laboratório</td>
-                  <td className="py-2 px-4 text-center border-b border-grey-light text-green-500">Ativo</td>
-                  <td className="flex py-2 px-4 text-center border-b border-grey-light"><IconEdit size={25} /><IconTrash size={40} /></td>
+                  <td className="py-2 px-4 text-center border-b border-grey-light text-green-500">
+                    <div className="flex items-center justify-center">
+                      <IconCerto size={16} />
+                      <span className="ml-1">Ativo</span>
+                    </div>
+                  </td>
+                  <td className="flex items-center justify-center py-2 px-4 text-center border-b border-grey-light">
+                    <div className="flex items-center space-x-2">
+                      <IconEdit size={30} />
+                      <div className="w-px h-7 bg-gradient-to-r from-cyan-300 to-cyan-500"></div>
+                      <IconTrash size={37} />
+                    </div>
+                  </td>
                 </tr>
               </tbody>
             </table>
