d849be839462d5e22af407e0780fbee3fc6b8dad
Joao Pedro Neris <joaopedrojesus331@gmail.com>
2023-11-28 14:18:01 -0300

    pequenas alterações

--- src/components/Nah_Salas/index.tsx
+++ src/components/Nah_Salas/index.tsx
@@ -5,8 +5,8 @@ import { RiEmotionSadFill } from "react-icons/ri";
 const NahSalas: React.FC = () => {
   return (
     <div className="flex flex-col items-center mt-8">
-      <RiEmotionSadFill color="#05AFF2" size={30}/>
-      <p className="mt-2 text-gray-500 dark:text-gray-400">Não há doorsenses cadastrados.</p>
+      <RiEmotionSadFill color="#05AFF2" size={50}/>
+      <p className="mt-2 text-gray-500 dark:text-gray-400">Não há salas cadastrados.</p>
     </div>
   );
 };
